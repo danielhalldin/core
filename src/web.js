7d2ef4fb9cc3677e02d4c66b0eeb82e747a734bc
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2018-10-01 21:33:39 +0200

    Various

--- src/web.js
+++ src/web.js
@@ -6,6 +6,7 @@ import { RedisCache } from "apollo-server-cache-redis";
 import omdbAPI from "./grapql/dataSources/omdbApi";
 import untappdAPI from "./grapql/dataSources/untappdAPI";
 import elasticsearchAPI from "./grapql/dataSources/elasticsearchAPI";
+import IndexClient from "./lib/worker/indexClient";
 import schema from "./grapql/schema";
 import config from "./config";
 import fetch from "node-fetch";
@@ -52,7 +53,7 @@ async function run() {
     res.redirect(
       `https://untappd.com/oauth/authenticate/?client_id=${
         config.untappedClientID
-      }&response_type=code&redirect_url=http%3A%2F%2Flocalhost%3A4444%2Fauth`
+      }&response_type=code&redirect_url=http%3A%2F%2Fdata-source.ddns.net%2Fauth`
     );
   });
 
@@ -62,10 +63,30 @@ async function run() {
       config.untappedClientID
     }&client_secret=${
       config.untappedClientSecret
-    }&response_type=code&redirect_url=http%3A%2F%2Flocalhost%3A4444%2Fauth&code=${code}`;
+    }&response_type=code&redirect_url=http%3A%2F%2Fdata-source.ddns.net%2Fauth&code=${code}`;
     const authorizeResponse = await fetch(url);
     const token = (await authorizeResponse.json()).response.access_token;
-    res.redirect(`http://localhost:3001/?token=${token}`);
+    res.redirect(`https://new-beers.herokuapp.com/?token=${token}`);
+  });
+
+  app.get("/manual-update", async function(req, res) {
+    const id = req.query.id;
+    const name = req.query.name;
+    const indexClient = new IndexClient();
+    const documentBody = {
+      Namn: name,
+      Namn2: null,
+      untappdId: null,
+      untappdData: null
+    };
+    const responseData = await indexClient.updateDocument({
+      index: "systembolaget",
+      type: "artikel",
+      id: id,
+      documentBody: documentBody
+    });
+
+    res.send(responseData);
   });
 
   https: server.applyMiddleware({ app });
