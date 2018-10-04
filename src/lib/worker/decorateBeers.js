5d04f9013e3261e3d91a6da3ecf562935ea85eaf
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2018-10-04 11:20:13 +0200

    Refactoring decorateBeers

---
+++ src/lib/worker/decorateBeers.js
@@ -0,0 +1,70 @@
+import _ from "lodash";
+import logger from "../logger";
+
+const tidyQuery = query => {
+  query = query
+    .toLowerCase()
+    .replace("ab", "")
+    .replace("aktiebryggeri", "")
+    .replace("ale", "")
+    .replace("&", "");
+
+  return _.uniq(query.split(" ")).join(" ");
+};
+
+const decorateBeers = async ({ indexClient, searchClient, untappdClient }) => {
+  const beersToDecorate = await searchClient.latatestBeersToBeDecorated({
+    size: 1
+  });
+
+  if (beersToDecorate.length === 0) {
+    return;
+  }
+
+  logger.info("Running decorateWithUntappd");
+
+  beersToDecorate.map(async beer => {
+    const name = `${beer._source.Namn}${
+      beer._source.Namn2 ? " " + beer._source.Namn2 : ""
+    }`;
+
+    try {
+      const query1 = tidyQuery(`${beer._source.Producent} ${name}`);
+      logger.info("query1: " + query1);
+      const untappdSearchResult = await untappdClient.searchBeer(query1);
+
+      let untappdData;
+      if (untappdSearchResult.length > 0) {
+        untappdData = untappdSearchResult[0];
+      } else {
+        const query2 = tidyQuery(name.replace(beer._source.Producent, ""));
+        logger.info("query2: " + query2);
+        const untappdSearchResult = await untappdClient.searchBeer(query2);
+        if (untappdSearchResult.length > 0) {
+          untappdData = untappdSearchResult[0];
+        }
+      }
+
+      let documentBody = {
+        untappdId: 0
+      };
+      if (untappdData) {
+        documentBody = {
+          untappdId: untappdData.beer.bid,
+          untappdData: untappdData
+        };
+      }
+
+      return indexClient.updateDocument({
+        index: "systembolaget",
+        type: "artikel",
+        id: beer._id,
+        documentBody: documentBody
+      });
+    } catch (e) {
+      logger.error("Failed to decorate beer: " + e.message);
+    }
+  });
+};
+
+export default decorateBeers;
--- src/worker.js
+++ src/worker.js
@@ -1,16 +1,23 @@
 import IndexClient from "./lib/worker/clients/indexClient";
-import beerIndexer from "./lib/worker/beerIndexer";
-import BeerDecorator from "./lib/worker/beerDecorator";
+import SearchClient from "./lib/worker/clients/searchClient";
+import UntappdClient from "./lib/worker/clients/untappdClient";
+
+import indexBeers from "./lib/worker/indexBeers";
+import decorateBeers from "./lib/worker/decorateBeers";
 
 import config from "./config";
 
-var indexClient = new IndexClient();
-var beerDecorator = new BeerDecorator();
+const indexClient = new IndexClient();
+const searchClient = new SearchClient();
+const untappdClient = new UntappdClient();
 
 indexClient.healthCheck(60000);
-beerIndexer(indexClient);
 
 // Indexing
+indexBeers(indexClient);
 
 // Decorating
-setInterval(beerDecorator.decorateWithUntappd, config.decoratorInterval);
+setInterval(
+  () => decorateBeers({ indexClient, searchClient, untappdClient }),
+  config.decoratorInterval
+);
