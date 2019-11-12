830d1eec208b7414b9a9460e940e6a3e9cf4d608
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2019-11-12 21:51:13 +0100

    Documentation

--- src/grapql/typeDefs.js
+++ src/grapql/typeDefs.js
@@ -9,22 +9,57 @@ export default gql`
     The lates [size] beers of the [stockType] decoreted with untappd data
     """
     decoratedLatest(size: Int, stockType: String): list
+
     """
     The recommended [size] beers
     """
     recommended(size: Int): list
 
+    """
+    The [size] latest beers from systembolaget
+    """
     systembolagetLatest(size: Int): [beer]
+
+    """
+    Search untappd using the [id]
+    """
     untappdById(id: ID!): beer
+
+    """
+    The users friends
+    """
     untappdFriends: [user]
+
+    """
+    Check if the user is a friend to the admin
+    """
     untappdIsFriend: Boolean
+
+    """
+    Search untappd using a [query]
+    """
     untappdSearch(query: String!): [beer]
+
+    """
+    The untappd user
+    """
     untappdUser: user
+
+    """
+    The users latest checkins
+    """
     untappdUserBeers: [beer]
   }
 
   type Mutation {
+    """
+    Delete the beer with [systembolagetArticleId]
+    """
     deleteBeer(systembolagetArticleId: Int!): Boolean
+
+    """
+    Set [untappdId] on beer with [systembolagetArticleId]
+    """
     updateUntappdId(systembolagetArticleId: Int!, untappdId: Int!): Boolean
   }
 `;
