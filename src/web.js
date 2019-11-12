c9f85c2db4ff59e43af00c50b56e073b2b9a43f2
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2019-11-12 20:36:43 +0100

    remove throng

--- src/web.js
+++ src/web.js
@@ -3,7 +3,6 @@ import { RedisCache } from "apollo-server-cache-redis";
 import compression from "compression";
 import express from "express";
 import schema from "./grapql/schema";
-import throng from "throng";
 import _get from "lodash/get";
 
 import untappdAPI from "./lib/web/dataSources/untappdAPI";
@@ -18,72 +17,61 @@ import { decrypt } from "./lib/jwtHandler";
 import logger from "./lib/logger";
 import morgan from "morgan";
 
-async function run() {
-  const redisCache = new RedisCache({
-    url: config.rediscloudUrl,
-    options: { no_ready_check: true }
-  });
+const redisCache = new RedisCache({
+  url: config.rediscloudUrl,
+  options: { no_ready_check: true }
+});
 
-  const server = new ApolloServer({
-    schema,
-    tracing: true,
-    cacheControl: true,
-    cache: redisCache,
-    engine: {
-      apiKey: process.env.ENGINE_API_KEY
-    },
-    persistedQueries: redisCache,
-    introspection: config.graphql.introspectionEnabled,
-    playground: config.graphql.playgroundEnabled,
-    dataSources: () => ({
-      UntappdAPI: new untappdAPI(),
-      ElasticsearchApi: new elasticsearchAPI()
-    }),
-    formatError: error => {
-      return {
-        message: error.message,
-        locations: error.locations,
-        stack: error.stack,
-        path: error.path
-      };
-    },
-    context: ({ req }) => {
-      const raw_untappd_access_token = _get(
-        req,
-        "headers.x-untappd-access-token"
-      );
-      const untappd_access_token = raw_untappd_access_token
-        ? decrypt(raw_untappd_access_token)
-        : "";
-      return {
-        untappd_access_token
-      };
-    }
-  });
-
-  const app = express();
-  app.use(compression());
-  app.use(morgan("dev"));
-  app.use(express.json());
-  app.use(express.static("public"));
-
-  // Routes
-  loginRoutes(app);
-  updateRoutes(app);
-  pushRoutes(app);
-
-  https: server.applyMiddleware({ app });
-
-  app.listen({ port: config.port }, () =>
-    logger.info(`Server is running on ${config.port}`)
-  );
-}
-
-const WORKERS = config.webConcurrenct || 1;
-throng(
-  {
-    workers: WORKERS,
-    lifetime: Infinity
+const server = new ApolloServer({
+  schema,
+  tracing: true,
+  cacheControl: true,
+  cache: redisCache,
+  engine: {
+    apiKey: process.env.ENGINE_API_KEY
   },
-  run
+  persistedQueries: redisCache,
+  introspection: config.graphql.introspectionEnabled,
+  playground: config.graphql.playgroundEnabled,
+  dataSources: () => ({
+    UntappdAPI: new untappdAPI(),
+    ElasticsearchApi: new elasticsearchAPI()
+  }),
+  formatError: error => {
+    return {
+      message: error.message,
+      locations: error.locations,
+      stack: error.stack,
+      path: error.path
+    };
+  },
+  context: ({ req }) => {
+    const raw_untappd_access_token = _get(
+      req,
+      "headers.x-untappd-access-token"
+    );
+    const untappd_access_token = raw_untappd_access_token
+      ? decrypt(raw_untappd_access_token)
+      : "";
+    return {
+      untappd_access_token
+    };
+  }
+});
+
+const app = express();
+app.use(compression());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Routes
+loginRoutes(app);
+updateRoutes(app);
+pushRoutes(app);
+
+server.applyMiddleware({ app });
+
+app.listen({ port: config.port }, () =>
+  logger.info(`Server is running on ${config.port}`)
 );
