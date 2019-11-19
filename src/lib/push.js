07a69ed8e7dfe0ffcded15c50162bc923c79f298
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2019-11-19 17:49:02 +0100

    Map sortiment to path for notifications

--- src/lib/push.js
+++ src/lib/push.js
@@ -1,6 +1,22 @@
 import webpush from 'web-push';
 import config from '../config';
 
+const mapSortimentToPath = sortiment => {
+  switch (sortiment) {
+    case 'Fast sortiment':
+      return '/fast-sortiment';
+    case 'Ordervaror':
+      return '/ordervaror';
+    case 'Lokalt & Småskaligt':
+      return '/lokalt-och-smaskaligt';
+    case 'Tillfälligt sortiment':
+      return '/tillfalligt-sortiment';
+    case 'Säsong':
+      return '/sasong';
+    default:
+      return '/rekommenderade';
+  }
+};
 export const send = async ({ redisClient, systembolagetData, untappdData }) => {
   if (!redisClient) {
     return;
@@ -10,7 +26,7 @@ export const send = async ({ redisClient, systembolagetData, untappdData }) => {
     title: `${untappdData.beer.beer_name} - ${untappdData.brewery.brewery_name}`,
     body: `[${systembolagetData._source.SortimentText}]`,
     icon: untappdData.beer.beer_label,
-    path: '/checkins'
+    data: { path: mapSortimentToPath(systembolagetData._source.SortimentText) }
   });
   let subscriptionsKeys = [];
   try {
