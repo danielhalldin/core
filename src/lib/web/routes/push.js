23e29f4f819ae97a35a0436bfab058ae280afe26
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2019-11-21 21:01:12 +0100

    use push-lib in push route

--- src/lib/web/routes/push.js
+++ src/lib/web/routes/push.js
@@ -1,41 +1,41 @@
 import _get from 'lodash/get';
-import config from '../../../config';
+import { send } from '../../push';
+import RedisClient from '../../worker/clients/redisClient';
 
-import webpush from 'web-push';
-import { set, get, keys } from '../../../lib/redisClient';
+const redisClient = new RedisClient();
 
-webpush.setVapidDetails(config.webPush.vapidEmail, config.webPush.vapidPublicKey, config.webPush.vapidPrivateKey);
-
-const push = app => {
+const pushRoute = app => {
   app.post('/subscribe', (req, res) => {
     const subscription = req.body;
     if (_get(subscription, 'keys.p256dh')) {
-      set(`subscription-${_get(subscription, 'keys.p256dh')}`, JSON.stringify(subscription), 'EX', 3600 * 24 * 365);
+      redisClient.set(
+        `subscription-${_get(subscription, 'keys.p256dh')}`,
+        JSON.stringify(subscription),
+        'EX',
+        3600 * 24 * 365
+      );
     }
     res.status(201).json({});
   });
 
   app.get('/push', async (req, res) => {
-    const payload = JSON.stringify({
-      title: 'Daniel testar lite',
-      body: 'Information om en ny öl ☀️',
-      data: { path: '/rekommenderade' },
-      icon: 'https://untappd.akamaized.net/site/beer_logos/beer-3092221_5cf17_sm.jpeg'
-    });
-    let subscriptionsKeys = [];
-    try {
-      subscriptionsKeys = await keys('subscription-*');
-      await subscriptionsKeys.map(async subscriptionKey => {
-        const subscription = JSON.parse(await get(subscriptionKey));
-        await webpush.sendNotification(subscription, payload).catch(error => {
-          console.error('webpush', error.stack);
-        });
-      });
-    } catch (e) {
-      console.error(e);
-    }
-    res.json(subscriptionsKeys).send(201);
+    const systembolagetData = {
+      _source: {
+        SortimentText: 'Tillfälligt sortiment'
+      }
+    };
+    const untappdData = {
+      beer: {
+        beer_name: 'Daniel testar lite',
+        beer_label: 'https://untappd.akamaized.net/site/beer_logos/beer-3092221_5cf17_sm.jpeg'
+      },
+      brewery: {
+        brewery_name: 'Information om en ny öl ☀️'
+      }
+    };
+    send({ redisClient, systembolagetData, untappdData });
+    res.json('Message sent').send(201);
   });
 };
 
-export default push;
+export default pushRoute;
