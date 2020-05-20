8d205852f9c444c7ad39a24e7e02ccd28a4d9c95
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2020-05-20 18:02:12 +0200

    Add return url to login

--- src/lib/web/routes/login.js
+++ src/lib/web/routes/login.js
@@ -1,15 +1,24 @@
 import querystring from 'querystring';
 import fetch from 'node-fetch';
-
+import cookie from 'cookie';
 import config from '../../../config';
 import { encrypt } from '../../jwtHandler';
 
-const login = app => {
-  app.get('/login', function(req, res) {
+const login = (app) => {
+  app.get('/login', function (req, res) {
+    if (req.query.returnUrl) {
+      res.setHeader(
+        'Set-Cookie',
+        cookie.serialize('returnUrl', String(req.query.returnUrl), {
+          httpOnly: true,
+          maxAge: 60 * 10, // 10 minutes
+        })
+      );
+    }
     const params = {
       client_id: config.untappd.clientID,
       response_type: 'code',
-      redirect_url: encodeURI(config.newBeers.authUrl)
+      redirect_url: encodeURI(config.newBeers.authUrl),
     };
 
     const url = config.untappd.authBaseUrl + '/authenticate/?' + querystring.stringify(params);
@@ -17,21 +26,24 @@ const login = app => {
     res.redirect(url);
   });
 
-  app.get('/auth', async function(req, res) {
+  app.get('/auth', async function (req, res) {
     const code = req.query.code;
     const params = {
       client_id: config.untappd.clientID,
       client_secret: config.untappd.clientSecret,
       response_type: 'code',
       redirect_url: encodeURI(config.newBeers.authUrl),
-      code: code
+      code: code,
     };
 
     const url = config.untappd.authBaseUrl + '/authorize/?' + querystring.stringify(params);
     const authorizeResponse = await fetch(url);
     const token = (await authorizeResponse.json()).response.access_token;
     const jwt = encrypt(token);
-    res.redirect(`${config.newBeers.url}/?token=${jwt}`);
+
+    const cookies = cookie.parse(req.headers.cookie || '');
+    const returnUrl = cookies.returnUrl || config.newBeers.url;
+    res.redirect(`${returnUrl}/?token=${jwt}`);
   });
 };
 
