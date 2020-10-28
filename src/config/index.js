8bce59ea0e145e309e3dfd1c64f7509698a47f3a
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2020-10-28 21:03:58 +0100

    Update to new Systembolaget api

--- src/config/index.js
+++ src/config/index.js
@@ -14,34 +14,35 @@ module.exports = {
     introspectionEnabled: _get(envs, 'INTROSPECTION_ENABLED') === 'true' || false,
     playgroundEnabled: _get(envs, 'PLAYGROUND_ENABLED') === 'true' || false,
     tracingEnabled: _get(envs, 'TRACING_ENABLED') === 'true' || false,
-    engineApiKey: _get(envs, 'ENGINE_API_KEY'),
+    engineApiKey: _get(envs, 'ENGINE_API_KEY')
   },
 
   rediscloudUrl: _get(envs, 'REDISCLOUD_URL'),
 
   newBeers: {
     url: _get(envs, 'NEW_BEERS_URL', 'http://new-beers.ddns.net'),
-    authUrl: _get(envs, 'NEW_BEERS_AUTH_URL', 'http://data-source.ddns.net/auth'),
+    authUrl: _get(envs, 'NEW_BEERS_AUTH_URL', 'http://data-source.ddns.net/auth')
   },
 
   elasticsearch: {
-    url: _get(envs, 'BONSAI_URL', 'http://localhost:9200'),
+    url: _get(envs, 'BONSAI_URL', 'http://localhost:9200')
   },
 
   untappd: {
     baseUrl: _get(envs, 'UNTAPPED_BASE_URL', 'https://api.untappd.com'),
     authBaseUrl: _get(envs, 'UNTAPPED_AUTH_BASE_URL', 'https://untappd.com/oauth'),
     clientID: _get(envs, 'UNTAPPED_CLIENT_ID'),
-    clientSecret: _get(envs, 'UNTAPPED_CLIENT_SECRET'),
+    clientSecret: _get(envs, 'UNTAPPED_CLIENT_SECRET')
   },
 
   systembolaget: {
-    url: _get(envs, 'SYSTEMBOLAGET_URL', 'http://www.systembolaget.se/api/assortment/products/xml'),
+    url: _get(envs, 'SYSTEMBOLAGET_URL', 'https://api-extern.systembolaget.se/product/v1/product'),
+    subscriptionKey: _get(envs, 'SYSTEMBOLAGET_SUBSCRIPTION_KEY')
   },
 
   webPush: {
     vapidPublicKey: _get(envs, 'VAPID_PUBLIC_KEY'),
     vapidPrivateKey: _get(envs, 'VAPID_PRIVATE_KEY'),
-    vapidEmail: _get(envs, 'VAPID_EMAIL'),
-  },
+    vapidEmail: _get(envs, 'VAPID_EMAIL')
+  }
 };
--- src/lib/worker/indexBeers.js
+++ src/lib/worker/indexBeers.js
@@ -1,11 +1,9 @@
 import fetch from 'node-fetch';
-import xml2js from 'xml2js';
+import format from 'date-fns/format';
 import logger from '../logger';
 import config from '../../config';
 
-var parser = new xml2js.Parser({ explicitArray: false });
-
-const indexBeers = async (indexClient) => {
+const indexBeers = async indexClient => {
   // // REMOVE AND CREATE INDEX
   // try {
   //   await indexClient.deleteIndex("systembolaget");
@@ -22,26 +20,43 @@ const indexBeers = async (indexClient) => {
 
   // Fetching
   logger.info('Fetching Systembolaget data');
-  const res = await fetch(config.systembolaget.url);
+  const res = await fetch(config.systembolaget.url, {
+    headers: { 'Ocp-Apim-Subscription-Key': config.systembolaget.subscriptionKey, 'Content-Type': 'application/json' }
+  });
+  const json = await res.json();
 
   // Parsing
   logger.info('Parsing Systembolaget data');
-  const xml = await res.text();
-  const json = await new Promise((resolve, reject) =>
-    parser.parseString(xml, (err, result) => {
-      if (err) reject(err);
-      else resolve(result);
-    })
-  );
   const indexTimestamp = Date.now();
-  const beers = json.artiklar.artikel
-    .filter(function (article) {
-      return article.Varugrupp && article.Varugrupp.toLocaleLowerCase().indexOf('öl') !== -1;
-    })
-    .map(function (article) {
-      article.id = article.Artikelid;
-      article.indexTimestamp = indexTimestamp;
-      return article;
+  const beers = json
+    .filter(item => item.Category === 'Öl')
+    .map(function(item) {
+      const salesStartDate = new Date(item.SellStartDate);
+      const output = {
+        id: item.ProductId,
+        nr: item.ProductNumber,
+        Artikelid: item.ProductId,
+        Varnummer: item.ProductNumberShort,
+        Namn: item.ProductNameBold,
+        Namn2: item.ProductNameThin,
+        Prisinklmoms: item.Price,
+        Volymiml: item.Volume,
+        Saljstart: format(salesStartDate, 'yyyy-MM-dd'),
+        Utgått: item.IsCompletelyOutOfStock,
+        Varugrupp: item.Category,
+        Typ: item.Type,
+        Stil: item.Style,
+        Ursprung: item.OriginLevel1,
+        Ursprunglandnamn: item.Country,
+        Producent: item.ProducerName,
+        Leverantor: item.SupplierName,
+        Alkoholhalt: item.AlcoholPercentaget,
+        Sortiment: item.Assortment,
+        SortimentText: item.AssortmentText,
+        indexTimestamp: indexTimestamp
+      };
+
+      return output;
     });
 
   // Indexing
